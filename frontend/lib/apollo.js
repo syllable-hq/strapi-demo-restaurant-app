bc0553c4926a20e1831dd5cf860bb8c7790c37ef
bnalvarado94 <bnalvarado94@gmail.com>
2019-07-22 16:01:47 -0600

    modify apollo lib to point api url

--- frontend/lib/apollo.js
+++ frontend/lib/apollo.js
@@ -5,7 +5,7 @@ import { withData } from "next-apollo";
 
 const config = {
   link: new HttpLink({
-    uri: "http://localhost:1337/graphql", // Server URL (must be absolute)
+    uri: `${process.env.API_URL || 'http://localhost:1337'}/graphql`, // Server URL (must be absolute)
   })
 };
 export default withData(config);
